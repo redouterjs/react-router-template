f5bcea1c2eb78dfaaa16675282cdcd05c10e544e
Benjamin Goh <masotime@gmail.com>
2016-10-18 06:30:16 -0700

    Remove dependency on history, use react-router history instead

--- src/browser.js
+++ src/browser.js
@@ -1,8 +1,7 @@
 /* global document */
 import React from 'react';
 import { render } from 'react-dom';
-import createHistory from 'history/createBrowserHistory';
-import { Router, useRouterHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import Promise from 'bluebird';
 
 const noOp = _ => _;
@@ -12,7 +11,7 @@ export default function ({ target = document, wrapComponent = noOp, routes, crea
 	return async ({ history } = {}) => new Promise((ok, fail) => {
 		try {
 			if (!history) {
-				history = useRouterHistory(createHistory)();
+				history = browserHistory;
 			}
 
 			let output; // ugly, but oh well
